020c7d75b3a186ccc20393ac14842676cb2577f8
ndnestor <Nathan.nestor@outlook.com>
2021-05-26 00:07:49 -0400

    Finished full WebCrawler life cycle

--- Backend/WebCrawler.cs
+++ Backend/WebCrawler.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
+using System.Runtime.InteropServices;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
@@ -14,21 +16,71 @@ namespace Backend {
 		private const int TimeoutTime = 10; // In seconds
 		// The HTML class used to determine whether a page is loaded or not
 		private const string LoadedCssIndicator = "div[class='b_antiTopBleed b_antiSideBleed b_antiBottomBleed']";
+		// Firefox input argument(s). Note that "--headless" argument will cause an exception
+		private const string Arguments = "--headless screenshot";
 
-		// Call this before using any other methods in this class
-		public static void Initialize() {
-			_webDriver = new FirefoxDriver();
+		private static Process displayResultAhk;
+
+		private const int SW_HIDE = 0;
+		private const int SW_SHOW = 5;
+		[DllImport("User32")]
+		private static extern int ShowWindow(int hwnd, int nCmdShow);
+
+		// Call this before using any other methods in this class (returns false if it fails)
+		public static bool Initialize() {
+			try {
+				Console.WriteLine("Starting web crawler...");
+
+				// Create driver service
+				FirefoxDriverService driverService = FirefoxDriverService.CreateDefaultService();
+				driverService.HideCommandPromptWindow = true;
+
+				// Set options
+				FirefoxOptions options = new();
+				options.AddArguments(Arguments);
+				_webDriver = new FirefoxDriver(driverService, options);
+
+				// Hide window
+				HideWindow("Mozilla Firefox");
+
+				Console.WriteLine("Web crawler initialized");
+				return true;
+			} catch(Exception e) {
+				Console.WriteLine("Initialization failed with FATAL exception: " + e + "\n\nMake sure gecko driver is added to PATH");
+				return false;
+			}
+		}
+
+		// Runs an AHK script that hides the window. A substitute for running headless
+		private static void HideWindow(string windowTitle) {
+			Process hideWindowAhk = new();
+			hideWindowAhk.StartInfo.FileName = @"F:\OneDrive - Rutgers University\Programming\Bing Weather Display\Frontend\Hide Window.exe";
+			hideWindowAhk.StartInfo.Arguments = "\"" + windowTitle + "\"";
+			hideWindowAhk.Start();
+		}
+
+		// Runs an AHK script that shows the weather image
+		public static void DisplayResult() {
+			if(displayResultAhk != null) {
+				displayResultAhk.Kill();
+			}
+			displayResultAhk = new Process();
+			displayResultAhk.StartInfo.FileName = @"F:\OneDrive - Rutgers University\Programming\Bing Weather Display\Frontend\Display Image.exe";
+			displayResultAhk.Start();
 		}
 		
-		// Saves an image showing the weather forecast
-		public static Bitmap GetWeather(string city, string state) {
+		// Returns an image showing the weather forecast (returns null if it fails)
+		public static Bitmap GetWeather(string city, string state, bool saveResult=false) {
 			try {
 				// Navigate to the weather site
 				_webDriver.Navigate().GoToUrl(GetWeatherUrl(city, state));
 				IWebElement loadedElement = WaitForPage();
 				if(loadedElement == null) {
+					CloseBrowser();
 					return null;
 				}
+
+				HideWindow(_webDriver.Title + " — Mozilla Firefox");
 
 				// Take screenshot of page
 				byte[] screenshotByteArray = ((ITakesScreenshot)_webDriver).GetScreenshot().AsByteArray;
@@ -39,15 +91,21 @@ namespace Backend {
 					new(loadedElement.Location.X, loadedElement.Location.Y, loadedElement.Size.Width, loadedElement.Size
 						.Height);
 				screenshot = screenshot.Clone(cropArea, screenshot.PixelFormat);
-				screenshot.Save(@"C:\tmp\test.png", System.Drawing.Imaging.ImageFormat.Png);
 
-				// Close the browser
-				_webDriver.Close();
-				_webDriver.Quit();
+				if(saveResult) {
+					screenshot.Save(@"C:\tmp\Bing Weather.png", System.Drawing.Imaging.ImageFormat.Png);
+
+				}
+
+				Console.WriteLine("Succesfully took a screenshot of the Bing weather panel");
 
 				return screenshot;
 			} catch(Exception e) {
 				Console.WriteLine("Get weather failed with exception: " + e);
+
+				// Close the browser
+				CloseBrowser();
+
 				return null;
 			}
 		}
@@ -63,10 +121,16 @@ namespace Backend {
 				WebDriverWait driverWait = new(_webDriver, TimeSpan.FromSeconds(TimeoutTime));
 				driverWait.Until(driver => driver.FindElement(By.CssSelector(LoadedCssIndicator)));
 				return _webDriver.FindElement(By.CssSelector(LoadedCssIndicator));
-			} catch(NoSuchElementException e) {
+			} catch(Exception e) {
 				Console.WriteLine(e);
 				return null;
 			}
+		}
+
+		// Closes the Selenium browser
+		private static void CloseBrowser() {
+			_webDriver.Close();
+			_webDriver.Quit();
 		}
 	}
 }
